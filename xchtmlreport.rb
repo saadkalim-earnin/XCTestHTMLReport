08731e94b60a8c2b56e74fe15eeab4061e104a33
Saad Kalim <54158248+saadkalim-earnin@users.noreply.github.com>
2020-05-23 05:53:06 -0400

    Update xchtmlreport.rb

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -1,9 +1,9 @@
 class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
-  homepage "https://github.com/TitouanVanBelle/XCTestHTMLReport"
-  url "https://github.com/TitouanVanBelle/XCTestHTMLReport/archive/2.0.0.tar.gz"
+  homepage "https://github.com/saadkalim-earnin/XCTestHTMLReport.git"
+  url "https://github.com/saadkalim-earnin/XCTestHTMLReport/releases/download/3.0.0/xchtmlreport-3.0.0.zip"
   sha256 "4424d673d578e84e67fd96afa53a5bd3e80ec7acade65365a123af358c77b47e"
-  head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", :branch => "develop"
+  head "https://github.com/saadkalim-earnin/XCTestHTMLReport.git", :branch => "develop"
 
   def install
     system "swift build --disable-sandbox -c release"
